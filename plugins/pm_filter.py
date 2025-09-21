45ae5437ef735d46f05331873569cafbbaaab832
rishuraj7209910-rgb <rishu.raj7209910@gmail.com>
2025-09-21 21:23:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1537,7 +1537,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [
             [
                 InlineKeyboardButton(
-                    "ʀᴇᴘᴏ", url="https://github.com/JisshuTG/Jisshu-filter-bot"
+                    "creator", url="https://t.me/rishuBotz_Bot"
                 )
             ],
             [
