296a832114e0763083fc04e87537452ed6ecbfbb
rishuraj7209910-rgb <rishu.raj7209910@gmail.com>
2025-09-21 18:17:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ ADMINS = [
     for admin in environ.get("ADMINS", "6286894502").split()
 ]
 OWNER_USERNAME = environ.get(
-    "OWNER_USERNAME", "RishuBotz_Bot"
+    "OWNER_USERNAME", "Rishubhai1"
 )  # without @ or https://t.me/rishuBotz_Bot
 USERNAME = environ.get("USERNAME", "Rishubhai1")  # ADMIN USERNAME
 
